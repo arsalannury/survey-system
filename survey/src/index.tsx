b15e22431f90563af0c8e12e5053b3face1ffdc7
arsalannury <arsiiiland@yahoo.com>
2022-12-03 23:03:29 +0330

    🚧 create login UI start

--- survey/src/index.tsx
+++ survey/src/index.tsx
@@ -1,12 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <Router>
+    <App />
+  </Router>
+);
 
 // reportWebVitals();
