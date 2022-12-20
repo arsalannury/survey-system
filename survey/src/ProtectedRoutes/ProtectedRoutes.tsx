b1f65444a9e3fdc9b3e40edc72a9a928ca164526
arsalannury <arsiiiland@yahoo.com>
2022-12-20 23:18:34 +0330

    🎨 work on finally level of login supabase

--- survey/src/ProtectedRoutes/ProtectedRoutes.tsx
+++ survey/src/ProtectedRoutes/ProtectedRoutes.tsx
@@ -7,42 +7,50 @@ type Children = {
 };
 
 export const Protected = ({ children }: Children) => {
-  const [session, setSession] = useState<any>(null);
-
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
-      if(session) {
-        localStorage.setItem("survey-token-saved-local-storage-register-login-user",session?.access_token!)
+      if (session) {
+        localStorage.setItem(
+          'survey-token-saved-local-storage-register-login-user',
+          session?.access_token!
+        );
       }
       console.log(session);
     });
 
     supabase.auth.onAuthStateChange((_event, session) => {
-      if(session) {
-        localStorage.setItem("survey-token-saved-local-storage-register-login-user",session?.access_token!)
+      if (session) {
+        localStorage.setItem(
+          'survey-token-saved-local-storage-register-login-user',
+          session?.access_token!
+        );
       }
     });
   }, []);
   const isAuth = localStorage.getItem(
     'survey-token-saved-local-storage-register-login-user'
   );
-  return isAuth || session ? children : <Navigate to="/login-register-view" />;
+  return isAuth ? children : <Navigate to="/login-register-view" />;
 };
 
 export const ProtectedAuthPage = ({ children }: Children) => {
-  const [session, setSession] = useState<any>(null);
-
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
-      if(session) {
-        localStorage.setItem("survey-token-saved-local-storage-register-login-user",session?.access_token!)
+      if (session) {
+        localStorage.setItem(
+          'survey-token-saved-local-storage-register-login-user',
+          session?.access_token!
+        );
       }
       console.log(session);
     });
 
     supabase.auth.onAuthStateChange((_event, session) => {
-      if(session) {
-        localStorage.setItem("survey-token-saved-local-storage-register-login-user",session?.access_token!)
+      if (session) {
+        localStorage.setItem(
+          'survey-token-saved-local-storage-register-login-user',
+          session?.access_token!
+        );
       }
     });
   }, []);
@@ -50,5 +58,5 @@ export const ProtectedAuthPage = ({ children }: Children) => {
   const isAuth = localStorage.getItem(
     'survey-token-saved-local-storage-register-login-user'
   );
-  return isAuth || session ? <Navigate to="/" /> : children;
+  return isAuth ? <Navigate to="/" /> : children;
 };
